dbf4db656c8539930aeefd89cf59ec5e79b360a5
vesselofgod <vesselofgod@naver.com>
2017-02-01 19:38:00 +0900

    simple code

--- distance.ino
+++ distance.ino
@@ -5,8 +5,6 @@ void setup() {
 Serial.begin(9600);
 pinMode(trigPin,OUTPUT);
 pinMode(echoPin,INPUT);
-pinMode(4,OUTPUT);
-pinMode(5,OUTPUT);
 }
 
 void loop() {
@@ -18,20 +16,5 @@ duration=pulseIn(echoPin,HIGH);
 distance=(duration*340)/20000;
 Serial.println(distance);
 delay(100);
-if(distance>=100)
-{
-  analogWrite(5,255);
-  digitalWrite(4,LOW);
-}
-else if(50<=distance<100)
-{
-  analogWrite(5,150);
-  digitalWrite(4,LOW);
-}
-else
-{
-  analogWrite(5,0);
-  digitalWrite(4,LOW);  
-}
 }
 
